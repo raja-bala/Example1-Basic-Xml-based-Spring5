540c9d06a296cf6d4aec71b3bb85541a11198338
raja-bala <bharathiraja.techie@gmail.com>
2022-03-23 11:50:13 +0800

    Bean definition using multiple xml config

--- src/Application.java
+++ src/Application.java
@@ -1,17 +1,23 @@
+import com.github.DBConnect;
 import com.github.Person;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("daos.xml", "services.xml");
 
         Person p = context.getBean("person", Person.class);
 
         p.setFirstName("Raja");
         p.setLastName("Bala");
 
+        DBConnect dbConnect = context.getBean("dbconnect", DBConnect.class);
+
+        dbConnect.setDbConnection("Connection string");
+
         System.out.println(p.getFullName());
+        System.out.println(dbConnect.getDbConnection());
 
     }
 }
---
+++ src/com/github/DBConnect.java
@@ -0,0 +1,13 @@
+package com.github;
+
+public class DBConnect {
+    private String dbConnection;
+
+    public String getDbConnection() {
+        return dbConnection;
+    }
+
+    public void setDbConnection(String dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+}
